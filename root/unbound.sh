c09ca01f5b42de80a3ac0b2d312aa02465cdceac
madnuttah <96331755+madnuttah@users.noreply.github.com>
2021-12-28 17:23:16 +0100

    Update unbound.sh

--- root/unbound.sh
+++ root/unbound.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cp -a /dev/random /dev/urandom /dev/null /etc/unbound/dev.d/
-/usr/sbin/unbound-anchor -a /etc/unbound/iana.d/root.key
-exec /usr/sbin/unbound -d -c /etc/unbound/unbound.conf
+
+cp -a /dev/random /dev/urandom /dev/null /etc/unbound/unbound.d/
+/etc/unbound/unbound.d/sbin/unbound-anchor -a /etc/unbound/iana.d/root.key
+exec /etc/unbound/unbound.d/sbin/unbound -d -c /etc/unbound/unbound.conf
