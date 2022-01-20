99c1db28e4717d5428cd20eedf743191c0c5ea87
madnuttah <96331755+madnuttah@users.noreply.github.com>
2022-01-20 16:24:55 +0100

    Delete unbound.sh

--- root/unbound.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cp -a /dev/random /dev/urandom /dev/null /usr/local/unbound/unbound.d/
-/usr/local/unbound/unbound.d/sbin/unbound-anchor -a /usr/local/unbound/iana.d/root.key
-exec /usr/local/unbound/unbound.d/sbin/unbound -d -c /usr/local/unbound/unbound.conf
