b9f4b600845a59fe9ac20f485223e47e77a7af9a
madnuttah <96331755+madnuttah@users.noreply.github.com>
2022-02-04 16:12:15 +0100

    Delete root directory

--- root/unbound.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/usr/local/unbound/unbound.d/sbin/unbound-anchor -a /usr/local/unbound/iana.d/root.key
-exec /usr/local/unbound/unbound.d/sbin/unbound -d -c /usr/local/unbound/unbound.conf
