853336f3b8c646df6d8afe61e1b19a1d3e59b97b
ϺΛDИVTTΛH <96331755+madnuttah@users.noreply.github.com>
2023-01-12 12:45:51 +0100

    Improved healthcheck.sh
    Resolution should be handled by Unbound itself
    Added failure status message

--- unbound/root/healthcheck.sh
+++ unbound/root/healthcheck.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
-nslookup internic.net > /dev/null
+nslookup internic.net 127.0.0.1:53 > /dev/null
 STATUS=$?
 if [[ ${STATUS} -ne 0 ]]
 then
+    echo "DNS lookup failed"
     exit 1
 fi
