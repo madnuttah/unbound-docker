9ec5cee5b2d6210f93ace6e051a794a51d3d4817
ϺΛDИVTTΛH <mdntth@proton.me>
2024-02-20 13:11:05 +0100

    Update healthcheck.sh
    Fixed fixed logic error

--- unbound/root/healthcheck.sh
+++ unbound/root/healthcheck.sh
@@ -1,8 +1,7 @@
-#! /bin/sh
+#!/bin/sh
 netstat -ln | grep -c ":5335" &> /dev/null # Change ":5335" to the port you may use
 STATUS=$?
-#STATUS=0
-if [[ ${STATUS} -ge 1 ]]
+if [[ ${STATUS} -eq 0 ]]
 then
     exit 0
 fi
