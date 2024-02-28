3ee74ad4b2bb4b2dac34801e4bfe82a9de11e45f
ϺΛDИVTTΛH <mdntth@proton.me>
2024-02-28 18:31:00 +0100

    Manually Build Distroless Unbound Docker Image
    Added grep
    Improved and fixed healthcheck

--- unbound/root/healthcheck.sh
+++ unbound/root/healthcheck.sh
@@ -1,7 +1,31 @@
 #!/bin/sh
-netstat -ln | grep -c ":5335" &> /dev/null # Change ":5335" to the port you may use
-STATUS=$?
-if [[ ${STATUS} -eq 0 ]]
-then
+
+## Healthcheck
+
+PORT="5335" # Change to the port Unbound is listening on 127.0.0.1 (localhost)
+DOMAIN="unbound.net" # The domain/host to query in the extended check
+EXTENDED="0" # Change this to "1" if you would like to verify name resolution using drill
+
+CHECK_PORT="$(netstat -ln | grep -c ":$PORT")" &> /dev/null
+
+if [[ "$CHECK_PORT" -eq 0 ]]; then
+  echo "⚠️ Port $PORT not open"
+  exit 1
+else
+  echo "ℹ️ Port $PORT open"
+  if [[ "$EXTENDED" = "0" ]]; then
     exit 0
+  fi
+fi
+
+## Extended healthcheck
+
+IP="$(drill -Q -p $PORT $DOMAIN @127.0.0.1)" &> /dev/null
+
+if [[ $? -ne 0 ]]; then
+  echo "⚠️ Domain '$DOMAIN' not resolved"
+  exit 1 
+else
+  echo "ℹ️ Domain '$DOMAIN' resolved to '$IP'"
+  exit 0
 fi
