493f02ad8803687ebf8cb394e103f10d90456526
ϺΛDИVTTΛH <96331755+madnuttah@users.noreply.github.com>
2024-02-29 01:15:28 +0100

    Improvements
    Documented script
    Changed success icons
    
    Signed-off-by: ϺΛDИVTTΛH <96331755+madnuttah@users.noreply.github.com>

--- unbound/root/healthcheck.sh
+++ unbound/root/healthcheck.sh
@@ -2,17 +2,28 @@
 
 ## Healthcheck
 
-PORT="5335" # Change to the port Unbound is listening on 127.0.0.1 (localhost)
-DOMAIN="unbound.net" # The domain/host to query in the extended check
-EXTENDED="0" # Change this to "1" if you would like to verify name resolution using drill
+# Change to the port Unbound is listening on 127.0.0.1 (localhost)
+PORT="5335"
 
+# Change this to "1" if you would like to verify name resolution using drill
+# in the extended healthcheck
+EXTENDED="0"
+
+# The domain/host to query in the extended check
+DOMAIN="unbound.net"
+
+# Check for opened tcp/udp port(s) with netstat, grep port count and save
+# the result into a variable
 CHECK_PORT="$(netstat -ln | grep -c ":$PORT")" &> /dev/null
 
+# Opened port count should be larger than 0, otherwise an error in the
+# unbound.conf is likely
 if [[ "$CHECK_PORT" -eq 0 ]]; then
   echo "⚠️ Port $PORT not open"
   exit 1
 else
-  echo "ℹ️ Port $PORT open"
+  echo "✅ Port $PORT open"
+# Exit gracefully if EXTENDED=0
   if [[ "$EXTENDED" = "0" ]]; then
     exit 0
   fi
@@ -20,12 +31,16 @@ fi
 
 ## Extended healthcheck
 
+# Use NLnet Labs drill to query localhost for a domain/host and save the result
+# into a variable
 IP="$(drill -Q -p $PORT $DOMAIN @127.0.0.1)" &> /dev/null
 
+# Check the errorlevel of the last command, if larger than 0 something with the 
+# internet connection doesn't seem right
 if [[ $? -ne 0 ]]; then
   echo "⚠️ Domain '$DOMAIN' not resolved"
   exit 1 
 else
-  echo "ℹ️ Domain '$DOMAIN' resolved to '$IP'"
+  echo "✅️ Domain '$DOMAIN' resolved to '$IP'"
   exit 0
 fi
