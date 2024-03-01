1bb5faa7115a45b3f69b18cf0c3faab7faaf311d
ϺΛDИVTTΛH <mdntth@proton.me>
2024-03-01 08:29:12 +0100

    Pre Flight Check
    Added workflow to test signing with bot
    Improved redis healthcheck

--- doc/examples/redis/healthcheck.sh
+++ doc/examples/redis/healthcheck.sh
@@ -1,7 +1,10 @@
-#! /bin/sh
+#!/bin/sh
 
 SOCKET=/usr/local/unbound/cachedb.d/redis.sock
-if [[ ! -S "$SOCKET" ]];
-then
-    exit 1    
+if [[ ! -S "$SOCKET" ]]; then
+  echo "⚠️ Unix Socket not found"
+  exit 1    
+else 
+  echo "✅ Unix Socket found"
+  exit 0
 fi
